f555d4ab6f968a4bb8e2c3f79585c288d403b94f
mardzien <mardzien@gmail.com>
2020-05-03 23:49:53 +0200

    tags

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
 from django.urls import reverse
 
 
@@ -25,6 +26,7 @@ class Post(models.Model):
 
     objects = models.Manager()
     published = PublishedManager()
+    tags = TaggableManager()
 
     class Meta:
         ordering = ('-publish', )
