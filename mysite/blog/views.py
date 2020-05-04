37cd1869f194c65181ebd4baa722963245be2b75
mardzien <mardzien@gmail.com>
2020-05-04 21:23:48 +0200

    tags and filtering by tags

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,12 +2,19 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
+from taggit.models import Tag
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+    else:
+        tag = None
     paginator = Paginator(object_list, 3)
     page = request.GET.get('page')
     try:
@@ -18,7 +25,7 @@ def post_list(request):
         posts = paginator.pate(paginator.num_pages)
 
     # posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
+    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts, 'tag': tag})
 
 
 class PostListView(ListView):
