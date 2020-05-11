fa081c34cdd870377e2db6680fe5cc4e6cf42f17
mardzien <mardzien@gmail.com>
2020-05-11 23:04:31 +0200

    blog searching

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,9 +3,10 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector
 from taggit.models import Tag
 from .models import Post, Comment
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 
 
 def post_list(request, tag_slug=None):
@@ -76,3 +77,15 @@ def post_share(request, post_id):
         form = EmailPostForm()
         cd = {}
     return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent, 'cd': cd})
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.objects.annotate(search=SearchVector('title', 'body')).filter(search=query)
+    return render(request, 'blog/post/search.html', {'form': form, 'query': query, 'results': results})
